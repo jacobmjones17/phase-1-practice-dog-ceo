bf3c5ed0db450606306be2d74e01e4ccffdd420e
Jacob <jacobmjones17@gmail.com>
2022-03-23 15:20:00 -0400

    commit tree 4d5e5c26b3756243758d9f3b42ec7dc0ab27f4f7

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+let breeds;
+
+const dogImage = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+const imageObj = {
+    method: "GET",
+    headers: {
+        "Content-Type" : "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(),
+};
+
+
+function fetchImage() {
+    return fetch(dogImage)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(response) {
+        response.message.forEach(dog => showDog(dog))
+        // document.querySelector("body").textContent = object.id
+    })
+};
+
+function showDog(dog) {
+    const img = document.createElement("img")
+    img.src = dog
+    const dogImageContainer = document.getElementById("dog-image-container")
+    dogImageContainer.append(img)
+};
+
+
+function fetchBreeds() {
+    fetch(breedUrl)
+    .then(response => response.json()
+    .then(data => {
+        breeds = Object.keys(data.message);
+        breeds.forEach(breed => showBreeds(breed));
+        return breeds
+    }))
+
+    // debugger;
+};
+
+function showBreeds(breed) {
+    const li = document.createElement("li")
+    li.textContent = "breed"
+    li.addEventListener("click", (event) => {
+        event.target.style.color = "red"
+    });
+
+    const ul = document.getElementById("dog-breeds")
+    ul.append(li)
+};
+
+
+
+
+
+fetchImage();
+fetchBreeds()
